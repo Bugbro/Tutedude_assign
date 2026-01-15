880bbbdcf52bf3b256021fbf90da7058dee08b1d
Prabhat Pal <ppal6060@gmail.com>
2026-01-15 23:04:54 +0530

    Assignment Navbar ready

---
+++ assign4/script.js
@@ -0,0 +1,13 @@
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", ()=>{
+    navMenu.classList.toggle("left-[0]");
+});
+
+navLink.forEach(link =>{
+    link.addEventListener("click", ()=>{
+        navMenu.classList.toggle("left-[0]");
+    })
+})
